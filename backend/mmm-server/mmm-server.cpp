1a3812fd563455ee71e742b7322c981d67e9d90a
Tim Weißker <tim.weissker@uni-weimar.de>
2015-04-11 18:09:05 +0200

    Implement WebSocket handshake in order to allow connections from browser.

--- backend/mmm-server/mmm-server.cpp
+++ backend/mmm-server/mmm-server.cpp
@@ -5,6 +5,8 @@
 #include <map>
 
 #include "PlayerConnection.hpp"
+#include "base64.hpp"
+#include "sha1.hpp"
 
 int main()
 {
@@ -41,8 +43,61 @@ int main()
 		        PlayerConnection* playerConnection = new PlayerConnection(globalConnectionCounter, address, client);
 		        socketPlayerConnetions.insert(std::make_pair(client, playerConnection));
 		        selector.Add(client);
+		    	
+		    	/* Perform WebSocket handshake */
 
-		        std::cout << "Client " << globalConnectionCounter << " (" << address << ") connected." << std::endl;
+		    	//receive request
+		    	char requestBuffer[1024];
+		    	std::size_t receivedSize;
+		    	
+		    	if (client.Receive(requestBuffer, sizeof(requestBuffer), receivedSize) != sf::Socket::Done)
+		    	{
+		    		std::cout << "Error: Receiving request failed." << std::endl;
+		    	}
+
+		    	std::string request(requestBuffer);
+
+		    	//find Sec-WebSocket-Key
+		    	std::string searchString("Sec-WebSocket-Key: ");
+
+		    	std::size_t stringPosition = request.find(searchString);
+		    	std::size_t endPosition = request.find("\r\n", stringPosition);
+		    	std::string socketKey = request.substr(stringPosition + searchString.size(), 
+		    		                                   endPosition-stringPosition-searchString.size());
+
+		    	//generate Sec-WebSocket-Accept
+		    	std::string globallyUniqueIdentifier("258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+		    	std::stringstream concatStream;
+		    	concatStream << socketKey << globallyUniqueIdentifier;
+		    	std::string concatenatedString = concatStream.str();
+
+	    		unsigned char hash[20];
+				sha1::calc(concatenatedString.c_str(), concatenatedString.size(), hash);
+				std::string secWebSocketAccept = base64_encode(reinterpret_cast<const unsigned char*>(hash), 20);
+				
+				//send acceptance response
+				std::stringstream response;
+				response << "HTTP/1.1 101 Switching Protocols\r\n";
+				response << "Upgrade: websocket\r\n";
+				response << "Connection: Upgrade\r\n";
+				response << "Sec-WebSocket-Accept: " << secWebSocketAccept << "\r\n\r\n";
+
+				std::string responseString = response.str();
+		    	char responseBuffer[responseString.size()]; 
+		    	
+		    	for (int i = 0; i < responseString.size(); ++i)
+		    	{
+		    		responseBuffer[i] = responseString[i];
+		    	}
+
+		    	if (client.Send(responseBuffer, sizeof(responseBuffer)) != sf::Socket::Done)
+		    	{
+		    		std::cout << "Error: Sending response failed." << std::endl;
+		    	}
+
+		    	//client is now connected
+		    	std::cout << "Client " << globalConnectionCounter << " (" << address << ") connected." << std::endl;
+
 		    }
 
 		    //else it is a client socket, so we read the message which was sent
