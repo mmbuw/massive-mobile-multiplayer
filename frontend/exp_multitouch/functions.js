2d1f5c75a53ac0ce14a76f66610b96aed432a632
ridicul0u5 <andreas.berst@uni-weimar.de>
2015-04-20 14:47:03 +0200

    bugfix

--- frontend/exp_multitouch/functions.js
+++ frontend/exp_multitouch/functions.js
@@ -1,99 +1,132 @@
 
 window.addEventListener('load', function(){
- 		// Websocket
+
+		// Websocket
 		var socket = new WebSocket("ws://29.4.93.1:53000");
  
 		// Nach dem öffnen des Sockets den Status anzeigen
 		socket.onopen 	= function() 	{ console.log('Socket Status: '+socket.readyState+' (open)');	}
 
+
+ 		//##################################################################################
  		//button control
 	    var button = document.getElementById('button');
 
 	 
 	    button.addEventListener('touchstart', function(e){
-	        socket.send('A');
-	        console.log('pushbutton-start');
+
+	        button.style.backgroundColor = '#000000';
 	       	e.preventDefault();
+
+	       	//sent to server
+	       	socket.send('A');
+
+	       	//console debug
+	       	console.log('pushbutton-start');
 	    }, false)
 	 
 	    button.addEventListener('touchend', function(e){
-
-	        console.log('pushbutton-end');
+	    	button.style.backgroundColor = '#cccccc';
 	       	e.preventDefault();
+
+	       	//console debug
+	       	console.log('pushbutton-end');
 	    }, false)
 
+		//##################################################################################
 	    //steuernippel
+	    var wrapper = document.getElementById('canvas-wrap')
  		var canvas = document.getElementById('sadstick');
  		var circle = document.getElementById('circle');
 
-
+ 		//initialize canvas with and height
  		var context = canvas.getContext('2d');
- 		var centerX = canvas.offsetWidth / 2;
- 		var centerY = canvas.offsetHeight / 2;
+ 		canvas.width =  wrapper.offsetWidth;
+ 		canvas.height = wrapper.offsetHeight;
 
+ 		//initialize midpoint
+ 		var centerX = canvas.width /2;
+ 		var centerY = canvas.height /2;
+
+ 		//initialize vars
  		var startx = 0;
-	        var starty = 0;
-	        var diffx = 0;
-	        var diffy = 0;
+	    var starty = 0;
+	    var diffx = 0;
+	    var diffy = 0;
 
-	        //set circle in center
+	    //blurr fix
+	   	context.translate(0.5, 0.5);
+
+	    //set circle in center
  		circle.style.top = centerY - circle.offsetHeight/2;
  		circle.style.left = centerX -circle.offsetWidth/2;
 
  		//start touchinteraction on circle
- 	    circle.addEventListener('touchstart', function(e){
+ 		circle.addEventListener('touchstart', function(e){
 	        var touchobj = e.changedTouches[0];
 	        startx = parseInt(touchobj.clientX);
-	        starty = parseInt(touchobj.clientY);
+	        starty = parseInt(touchobj.clientX);
 	       	e.preventDefault();
 
+	       	//sent to server
 	       	socket.send(startx+' '+starty);
+
+	       	//console debug
 	       	console.log('start'+startx+'/'+starty);
 	    }, false)
 
- 		//move circle through touch
+ 		//move circle and update canvas
 	   	circle.addEventListener('touchmove', function(e){
 
 	        var touchobj = e.changedTouches[0];
 	        var currx = parseInt(touchobj.clientX);
 	        var curry = parseInt(touchobj.clientY);
 
-
+	        //clear canvas
 	    	context.clearRect(0, 0, canvas.width, canvas.height);
 
-	    	context.lineWidth = 2;
+	    	//line properties
+	    	context.lineWidth = 4;
+	    	context.shadowColor = "transparent";
+	    	
+	    	//draw line from center tu crrent position
 	    	context.beginPath();
-
- 			context.moveTo(centerX, centerY);
+ 			context.moveTo(centerX,centerY);
 	    	context.lineTo(currx, curry);
 	    	context.closePath();
 	    	context.stroke();
 
-
+	    	//calc movement
 	    	diffx = currx - startx;
 	    	diffy = curry - starty;
+
+	    	//set circle to new position
 	        circle.style.top = centerY - circle.offsetHeight/2 + diffy;
  			circle.style.left = centerX - circle.offsetWidth/2 + diffx;
+
 	       	e.preventDefault();
 
+	       	//sent to server
+	       	socket.send(currx + ' ' + curry);
 
+	       	//console debug
 	    	console.log('start: '+centerX+'/'+centerY);
-
 	       	console.log('lineTo: '+currx+'/'+curry);
-
-	        socket.send(currx + ' ' + curry);
 
 
 	    }, false)
 
+	   	//touchend and reset joystick
 	    circle.addEventListener('touchend', function(e){
+
+	    	//set circle to start
 	        circle.style.top = centerY - circle.offsetHeight/2;
  			circle.style.left = centerX -circle.offsetWidth/2;
+
+ 			//clear canvas
  			context.clearRect(0, 0, canvas.width, canvas.height);
 	       	e.preventDefault();
 	    }, false)
-
-
 
 
 }, false)
