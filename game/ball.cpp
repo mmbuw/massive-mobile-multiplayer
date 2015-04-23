850ef8ebc8bc4a46ee44e419deac8c51fc3d9d00
Tim Weißker <tim.weissker@uni-weimar.de>
2015-04-23 13:28:09 +0200

    Implement ball border absorption.

--- game/ball.cpp
+++ game/ball.cpp
@@ -16,21 +16,35 @@ Ball::Ball() : PhysicalObject(0.6, 0, 0)
 
 /* virtual */ void Ball::clampPosition()
 {
-	if (posX_ < 105)
+	float borderAbsorptionCoefficient(0.7);
+
+	if (posX_ < 105) // left border
 	{
 		setPosition(105, posY_);
+		velX_ = -velX_;
+		velX_ *= borderAbsorptionCoefficient;
+		velY_ *= borderAbsorptionCoefficient;
 	}
-	else if (posX_ > 1820)
+	else if (posX_ > 1820) // right border
 	{
 		setPosition(1820, posY_);
+		velX_ = -velX_;
+		velX_ *= borderAbsorptionCoefficient;
+		velY_ *= borderAbsorptionCoefficient;
 	}
 
-	if (posY_ < 26)
+	if (posY_ < 26) // top border
 	{
 		setPosition(posX_, 26);
+		velY_ = -velY_;
+		velX_ *= borderAbsorptionCoefficient;
+		velY_ *= borderAbsorptionCoefficient;
 	}
-	else if(posY_ > 1150)
+	else if(posY_ > 1150) //bottom border
 	{
 		setPosition(posX_, 1150);
+		velY_ = -velY_;
+		velX_ *= borderAbsorptionCoefficient;
+		velY_ *= borderAbsorptionCoefficient;
 	}
 }
