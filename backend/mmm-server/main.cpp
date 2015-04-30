be2a92a7696c6cf0e4e3e2dbd93c054ebbf800e3
Tim Weißker <tim.weissker@uni-weimar.de>
2015-04-30 19:43:54 +0200

    Use SFML 2.0 socket selectors in mmm-server.

--- backend/mmm-server/main.cpp
+++ backend/mmm-server/main.cpp
@@ -23,7 +23,7 @@ int main()
 {
 	bool running(true);
 	std::map<int, PlayerConnection*> currentPlayerConnections;
-	int frameDurationNanoSec(1.0/tickFPS * 1000000000);
+	sf::SocketSelector socketSelector;
 
 	//information needed for WebSocket handshake (RFC 6455)
 	std::string globallyUniqueIdentifier("258EAFA5-E914-47DA-95CA-C5AB0DC85B11"); //RFC 4122
@@ -31,7 +31,6 @@ int main()
 
 	/* set up network communication */
 	sf::TcpListener listener;
-	listener.setBlocking(false);
 
 	//bind the listener to a port
 	if (listener.listen(port) != sf::Socket::Done)
@@ -40,6 +39,8 @@ int main()
 		return 1;
 	}
 
+	socketSelector.add(listener);
+
 	std::cout << std::endl;
 	std::cout << "#####################################" << std::endl;
 	std::cout << "# MMM - Game Controller Server         " << std::endl;
@@ -47,241 +48,234 @@ int main()
 	std::cout << "#####################################" << std::endl;
 	std::cout << std::endl;
 
-	Clock::time_point lastFrameTime = Clock::now();
-
 	while (running)
 	{
-		//check for a new connection
-		sf::TcpSocket* newClientSocket = new sf::TcpSocket;
-		newClientSocket->setBlocking(false);
-		bool deleteNewClientSocket(false);
-
-		if (listener.accept(*newClientSocket) == sf::Socket::Done)
+		//wait until the listener or one client socket is ready
+		if (socketSelector.wait())
 		{
-			std::stringstream responseStream;
-
-			//do not allow multiple connections by same host
-			if (currentPlayerConnections.find(newClientSocket->getRemoteAddress().toInteger()) != currentPlayerConnections.end())
+			//check listener
+			if (socketSelector.isReady(listener))
 			{
-				responseStream << "HTTP/1.1 403 Forbidden\r\n\r\n";
-				deleteNewClientSocket = true;
-			}
-			else
-			{  	
-		        PlayerConnection* playerConnection = new PlayerConnection(newClientSocket);
-		    	
-		    	/* Perform WebSocket handshake (RFC 6455) */
+				sf::TcpSocket* newClientSocket = new sf::TcpSocket;
+				listener.accept(*newClientSocket);
 
-		    	//receive request
-		    	char requestBuffer[10000];
-		    	std::size_t receivedSize;
+				std::stringstream responseStream;
+
+				//do not allow multiple connections by same host
+				if (currentPlayerConnections.find(newClientSocket->getRemoteAddress().toInteger()) != currentPlayerConnections.end())
+				{
+					responseStream << "HTTP/1.1 403 Forbidden\r\n\r\n";
+				}
+				else
+				{  	
+			        PlayerConnection* playerConnection = new PlayerConnection(newClientSocket);
+			    	
+			    	/* Perform WebSocket handshake (RFC 6455) */
+
+			    	//receive request
+			    	char requestBuffer[10000];
+			    	std::size_t receivedSize;
+			    	
+			    	if (newClientSocket->receive(requestBuffer, sizeof(requestBuffer), receivedSize) != sf::Socket::Done)
+			    	{
+			    		std::cout << "[Error] Receiving request failed." << std::endl;
+			    	}
+
+			    	std::string request(requestBuffer);
+
+			    	//std::cout << request << std::endl;
+
+			    	//find Sec-WebSocket-Key
+			    	std::size_t stringPosition = request.find(requestSearchString);
+			    	std::size_t endPosition = request.find("\r\n", stringPosition);
+			    	std::string secWebSocketKey = request.substr(stringPosition + requestSearchString.size(), 
+			    		                                         endPosition-stringPosition-requestSearchString.size());
+
+			    	//generate Sec-WebSocket-Accept
+			    	std::string concatenatedString = secWebSocketKey + globallyUniqueIdentifier;
+
+		    		unsigned char sha1Hash[20];
+					sha1::calc(concatenatedString.c_str(), concatenatedString.size(), sha1Hash);
+					std::string secWebSocketAccept = base64_encode(reinterpret_cast<const unsigned char*>(sha1Hash), 20);
+					
+					//send acceptance response
+					responseStream << "HTTP/1.1 101 Switching Protocols\r\n";
+					responseStream << "Upgrade: websocket\r\n";
+					responseStream << "Connection: Upgrade\r\n";
+					responseStream << "Sec-WebSocket-Accept: " << secWebSocketAccept << "\r\n\r\n";
+
+			    	//client is now connected
+			    	std::cout << "[Connect] " << playerConnection->getIP() << " (Client ID " << playerConnection->getID() << ")" << std::endl;
+			    	currentPlayerConnections.insert(std::make_pair(playerConnection->getIP().toInteger(), playerConnection));
+			    	socketSelector.add(*newClientSocket);
+		   		}
+
+			    //send response
+			    std::string responseStreamString = responseStream.str();
+		    	char responseStreamBuffer[responseStreamString.size()];
 		    	
-		    	if (newClientSocket->receive(requestBuffer, sizeof(requestBuffer), receivedSize) != sf::Socket::Done)
+		    	for (int i = 0; i < responseStreamString.size(); ++i)
 		    	{
-		    		std::cout << "[Error] Receiving request failed." << std::endl;
+		    		responseStreamBuffer[i] = responseStreamString[i];
 		    	}
 
-		    	std::string request(requestBuffer);
+		    	if (newClientSocket->send(responseStreamBuffer, sizeof(responseStreamBuffer)) != sf::Socket::Done)
+		    	{
+		    		std::cout << "[Error] Sending responseStream failed." << std::endl;
+		    	}
 
-		    	//std::cout << request << std::endl;
-
-		    	//find Sec-WebSocket-Key
-		    	std::size_t stringPosition = request.find(requestSearchString);
-		    	std::size_t endPosition = request.find("\r\n", stringPosition);
-		    	std::string secWebSocketKey = request.substr(stringPosition + requestSearchString.size(), 
-		    		                                         endPosition-stringPosition-requestSearchString.size());
-
-		    	//generate Sec-WebSocket-Accept
-		    	std::string concatenatedString = secWebSocketKey + globallyUniqueIdentifier;
-
-	    		unsigned char sha1Hash[20];
-				sha1::calc(concatenatedString.c_str(), concatenatedString.size(), sha1Hash);
-				std::string secWebSocketAccept = base64_encode(reinterpret_cast<const unsigned char*>(sha1Hash), 20);
-				
-				//send acceptance response
-				responseStream << "HTTP/1.1 101 Switching Protocols\r\n";
-				responseStream << "Upgrade: websocket\r\n";
-				responseStream << "Connection: Upgrade\r\n";
-				responseStream << "Sec-WebSocket-Accept: " << secWebSocketAccept << "\r\n\r\n";
-
-		    	//client is now connected
-		    	std::cout << "[Connect] " << playerConnection->getIP() << " (Client ID " << playerConnection->getID() << ")" << std::endl;
-		    	currentPlayerConnections.insert(std::make_pair(playerConnection->getIP().toInteger(), playerConnection));
-		    }
-
-		    //send response
-		    std::string responseStreamString = responseStream.str();
-	    	char responseStreamBuffer[responseStreamString.size()];
-	    	
-	    	for (int i = 0; i < responseStreamString.size(); ++i)
-	    	{
-	    		responseStreamBuffer[i] = responseStreamString[i];
-	    	}
-
-	    	if (newClientSocket->send(responseStreamBuffer, sizeof(responseStreamBuffer)) != sf::Socket::Done)
-	    	{
-	    		std::cout << "[Error] Sending responseStream failed." << std::endl;
-	    	}
-
-		}
-		else
-		{
-			//no new connection came in using newClientSocket
-			deleteNewClientSocket = true;
-		}
-
-		if (deleteNewClientSocket)
-		{
-			delete newClientSocket;
-		}
-
-		//check for new message and disconnection at each socket
-		for (std::map<int, PlayerConnection*>::iterator it = currentPlayerConnections.begin(); it != currentPlayerConnections.end(); )
-		{
-			PlayerConnection* playerConnection = it->second;
-
-			char receiveBuffer[100];
-	    	std::size_t receiveSize;
-			sf::Socket::Status status = playerConnection->getSocket()->receive(receiveBuffer, sizeof(receiveBuffer), receiveSize);
-
-			if (playerConnection->checkAlive() == false)
-			{
-				std::cout << "[Timeout] " << playerConnection->getSocket()->getRemoteAddress() << " (Client " << playerConnection->getID() << ")" << std::endl;
-				delete playerConnection;
-				it = currentPlayerConnections.erase(it);
 			}
-			else if (status == sf::Socket::NotReady)
-			{
-				++it;
-				continue;
-			}
-			else if (status == sf::Socket::Disconnected)
-			{
-				std::cout << "[Disconnect] " << playerConnection->getSocket()->getRemoteAddress() << " (Client " << playerConnection->getID() << ")" << std::endl;
-				delete playerConnection;
-				it = currentPlayerConnections.erase(it);
-			}
-			else
-			{
-				bool nextMessagePresent(false);
 
-	            int indexOfFirstMask(2);
-        		int indexOfFirstDataByte(indexOfFirstMask+4);
-        		int numDataBytes(receiveSize-indexOfFirstDataByte);
 
-				do
+			//check client sockets
+			for (std::map<int, PlayerConnection*>::iterator it = currentPlayerConnections.begin(); it != currentPlayerConnections.end(); )
+			{
+				PlayerConnection* playerConnection = it->second;
+
+				if (playerConnection->checkAlive() == false)
 				{
-			    	/* Decoding the client message using the WebSocket protocol */
-		            /* (http://stackoverflow.com/questions/8125507/how-can-i-send-and-receive-websocket-messages-on-the-server-side) */
+					std::cout << "[Timeout] " << playerConnection->getSocket()->getRemoteAddress() << " (Client " << playerConnection->getID() << ")" << std::endl;
+					socketSelector.remove(*(playerConnection->getSocket()));
+					delete playerConnection;
+					it = currentPlayerConnections.erase(it);
+				}
+				else if (socketSelector.isReady(*(playerConnection->getSocket())))
+				{
+					char receiveBuffer[100];
+			    	std::size_t receiveSize;
+					sf::Socket::Status status = playerConnection->getSocket()->receive(receiveBuffer, sizeof(receiveBuffer), receiveSize);
 
-		            //the bit masks used for decode WebSocket messages
-		            //unfortunately cannot be stored in a std::vector for some reason
-		            std::bitset<8> mask_0;
-		            std::bitset<8> mask_1;
-		            std::bitset<8> mask_2;
-		            std::bitset<8> mask_3;
+					if (status == sf::Socket::Disconnected)
+					{
+						std::cout << "[Disconnect] " << playerConnection->getSocket()->getRemoteAddress() << " (Client " << playerConnection->getID() << ")" << std::endl;
+						socketSelector.remove(*(playerConnection->getSocket()));
+						delete playerConnection;
+						it = currentPlayerConnections.erase(it);
+					}
+					else if (status == sf::Socket::Error)
+					{
+						std::cout << "[Error] " << playerConnection->getSocket()->getRemoteAddress() << " (Client " << playerConnection->getID() << ")" << std::endl;
+						socketSelector.remove(*(playerConnection->getSocket()));
+						delete playerConnection;
+						it = currentPlayerConnections.erase(it);
+					}
+					else
+					{
 
-		            //get decoding masks
-		            mask_0 = (std::bitset<8>) receiveBuffer[indexOfFirstMask];
-		            mask_1 = (std::bitset<8>) receiveBuffer[indexOfFirstMask+1];
-		            mask_2 = (std::bitset<8>) receiveBuffer[indexOfFirstMask+2];
-		            mask_3 = (std::bitset<8>) receiveBuffer[indexOfFirstMask+3];
+						bool nextMessagePresent(false);
 
-		            std::string message("");
+			            int indexOfFirstMask(2);
+		        		int indexOfFirstDataByte(indexOfFirstMask+4);
+		        		int numDataBytes(receiveSize-indexOfFirstDataByte);
 
-		            //decode data bytes with respective mask
-		            for (int i = indexOfFirstDataByte; i < receiveSize; ++i)
-		            {
-		            	long characterAsLong;
+						do
+						{
+					    	/* Decoding the client message using the WebSocket protocol */
+				            /* (http://stackoverflow.com/questions/8125507/how-can-i-send-and-receive-websocket-messages-on-the-server-side) */
 
-		            	if ( (i-indexOfFirstDataByte) % 4 == 0)
-		            		characterAsLong = ((std::bitset<8>) receiveBuffer[i] ^= mask_0).to_ulong();
-		            	else if ( (i-indexOfFirstDataByte) % 4 == 1)
-		            		characterAsLong = ((std::bitset<8>) receiveBuffer[i] ^= mask_1).to_ulong();
-		            	else if ( (i-indexOfFirstDataByte) % 4 == 2)
-		            		characterAsLong = ((std::bitset<8>) receiveBuffer[i] ^= mask_2).to_ulong();
-		            	else
-		            		characterAsLong = ((std::bitset<8>) receiveBuffer[i] ^= mask_3).to_ulong();
-		            
-		            	//check for end of message sign (sign .)
-		            	if (characterAsLong == (long) '.' && i != receiveSize-1)
-		            	{
-		            		nextMessagePresent = true;
-		            		indexOfFirstMask = i + 3;
-							indexOfFirstDataByte = indexOfFirstMask + 4;
-        					numDataBytes = receiveSize-indexOfFirstDataByte;
-        					message += (char) characterAsLong;
-		            		break;
-		            	}
-		            	else
-		            	{
-			            	nextMessagePresent = false;
-			            	message += (char) characterAsLong;
-		            	}
+				            //the bit masks used for decode WebSocket messages
+				            //unfortunately cannot be stored in a std::vector for some reason
+				            std::bitset<8> mask_0;
+				            std::bitset<8> mask_1;
+				            std::bitset<8> mask_2;
+				            std::bitset<8> mask_3;
 
-		            }
+				            //get decoding masks
+				            mask_0 = (std::bitset<8>) receiveBuffer[indexOfFirstMask];
+				            mask_1 = (std::bitset<8>) receiveBuffer[indexOfFirstMask+1];
+				            mask_2 = (std::bitset<8>) receiveBuffer[indexOfFirstMask+2];
+				            mask_3 = (std::bitset<8>) receiveBuffer[indexOfFirstMask+3];
 
-		            //print message
-		            //std::cout << "[Client " << playerConnection->getID() << "] " << message << std::endl;
+				            std::string message("");
 
-		            //react on messages by injecting keystrokes
-		            if (message == "VAL A$")
-		            {
-		            	//std::cout << "Inject key input event for A" << std::endl;
-		            	playerConnection->injectKeyEvent(BTN_A);
-		            }
-		            else if (message.find("NAME") == 0)
-		            {
-		            	std::stringstream stream(message);
-		            	std::string nameString, nameToSet;
+				            //decode data bytes with respective mask
+				            for (int i = indexOfFirstDataByte; i < receiveSize; ++i)
+				            {
+				            	long characterAsLong;
 
-		            	stream >> nameString;
-		            	stream >> nameToSet;
+				            	if ( (i-indexOfFirstDataByte) % 4 == 0)
+				            		characterAsLong = ((std::bitset<8>) receiveBuffer[i] ^= mask_0).to_ulong();
+				            	else if ( (i-indexOfFirstDataByte) % 4 == 1)
+				            		characterAsLong = ((std::bitset<8>) receiveBuffer[i] ^= mask_1).to_ulong();
+				            	else if ( (i-indexOfFirstDataByte) % 4 == 2)
+				            		characterAsLong = ((std::bitset<8>) receiveBuffer[i] ^= mask_2).to_ulong();
+				            	else
+				            		characterAsLong = ((std::bitset<8>) receiveBuffer[i] ^= mask_3).to_ulong();
+				            
+				            	//check for end of message sign (sign .)
+				            	if (characterAsLong == (long) '.' && i != receiveSize-1)
+				            	{
+				            		nextMessagePresent = true;
+				            		indexOfFirstMask = i + 3;
+									indexOfFirstDataByte = indexOfFirstMask + 4;
+		        					numDataBytes = receiveSize-indexOfFirstDataByte;
+		        					message += (char) characterAsLong;
+				            		break;
+				            	}
+				            	else
+				            	{
+					            	nextMessagePresent = false;
+					            	message += (char) characterAsLong;
+				            	}
 
-		            	std::cout << "[Client " << playerConnection->getID() << "] Assigning name: " << nameToSet.substr(0, nameToSet.size()-1) << std::endl;
-		            	playerConnection->setName(nameToSet.substr(0, nameToSet.size()-1));
-		            }
-		            else if (message.find("VAL") == 0)
-		            {
-		            	std::stringstream stream(message);
-		            	std::string nameString;
-		            	int x, y;
+				            }
 
-		            	stream >> nameString;
-		            	stream >> x;
-		            	stream >> y;
+				            //print message
+				            std::cout << "[Client " << playerConnection->getID() << "] " << message << std::endl;
 
-		            	if (x <= 1024 && y <= 1024 && x >= 0 && y >= 0)
-		            	{
-		            		playerConnection->injectRelEvent(x, y);
-		            		//std::cout << "Inject relative input event" << std::endl;
-		            	}
-		            }
-		            else
-		            {
-		            	std::cout << "[Client " << playerConnection->getID() << "] Omitting invalid message: " << message << std::endl;
-		            	nextMessagePresent = false;
-		            }
+				            //react on messages by injecting keystrokes
+				            if (message == "VAL A$")
+				            {
+				            	std::cout << "Inject key input event for A" << std::endl;
+				            	playerConnection->injectKeyEvent(BTN_A);
+				            }
+				            else if (message.find("NAME") == 0)
+				            {
+				            	std::stringstream stream(message);
+				            	std::string nameString, nameToSet;
 
-			    } while (nextMessagePresent);
+				            	stream >> nameString;
+				            	stream >> nameToSet;
+
+				            	std::cout << "[Client " << playerConnection->getID() << "] Assigning name: " << nameToSet.substr(0, nameToSet.size()-1) << std::endl;
+				            	playerConnection->setName(nameToSet.substr(0, nameToSet.size()-1));
+				            }
+				            else if (message.find("VAL") == 0)
+				            {
+				            	std::stringstream stream(message);
+				            	std::string nameString;
+				            	int x, y;
+
+				            	stream >> nameString;
+				            	stream >> x;
+				            	stream >> y;
+
+				            	if (x <= 1024 && y <= 1024 && x >= 0 && y >= 0)
+				            	{
+				            		playerConnection->injectRelEvent(x, y);
+				            		std::cout << "Inject relative input event" << std::endl;
+				            	}
+				            }
+				            else
+				            {
+				            	std::cout << "[Client " << playerConnection->getID() << "] Omitting invalid message: " << message << std::endl;
+				            	nextMessagePresent = false;
+				            }
+
+					    } while (nextMessagePresent);
+
+					} //if (status == ...)
+
+				} //if (socketSelector.isReady(...))
 
 		        //move to next socket
 				++it;
-			}
 
-		}
+			} // for each socket in map
 
-		//slow the application down to tickFPS
-		Clock::time_point end_time = Clock::now();
-		int currentNanoSec = (std::chrono::duration_cast<nanoseconds>(end_time - lastFrameTime)).count();
-		std::this_thread::sleep_for(std::chrono::nanoseconds(frameDurationNanoSec - currentNanoSec));
+		} //socketSelector.wait()
 
-		//fps output
-		//int frameDuration = (std::chrono::duration_cast<nanoseconds>(Clock::now() - lastFrameTime)).count();
-		//std::cout << "FPS: " << 1000000000.0/frameDuration << std::endl;
-
-		lastFrameTime = Clock::now();
-	}
+	} //while(running)
 
 	return 0;
 }
