081a71cef3c1dd14e3e4a97a3dfeb6c2a4a80b8c
Tim Weißker <tim.weissker@uni-weimar.de>
2015-05-07 12:34:39 +0200

    Remove call to pollEvent() as this slows application down on mouse moves.

--- game/main.cpp
+++ game/main.cpp
@@ -8,7 +8,7 @@
 
 int main()
 {
-	  srand (time(NULL));
+	srand (time(NULL));
 	sf::Clock Clock;
 
 	sf::RenderWindow App(sf::VideoMode(1920,1350,20), "MMMBall");
@@ -24,18 +24,17 @@ int main()
 	int frameCounterMod(0);
 	float lastFramerate(0.0);
 
+	sf::Event event;
+
 	while (App.isOpen()) 
 	{
-		sf::Event event;
-		App.clear(sf::Color(255,0,0));
+	
+		App.clear(sf::Color(255,255,0));
 
-		// handle events
-		while (App.pollEvent(event)) 
+		//handle events - do not use App.pollEvent(..) as it slows framerate on mouse move
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 		{
-			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) )
-			{ 
-				App.close();
-			}
+			App.close();
 		}
 
 		// input handling
@@ -52,17 +51,17 @@ int main()
 		// render output
 		game->renderBackground(&App);
 		game->renderGoals(&App);
-		game->renderSidelines(&App);
+	    game->renderSidelines(&App);
 		game->renderPlayers(&App);
 		game->renderBall(&App);
 		game->renderScoreLine(&App);
-
-		float framerate = 1.0f/Clock.getElapsedTime().asSeconds();
-		Clock.restart();
+		
 		frameCounterMod = (frameCounterMod + 1) % 5;
 		
 		if (frameCounterMod == 0)
 		{
+			float framerate = 5.0f/Clock.getElapsedTime().asSeconds();
+			Clock.restart();
 			game->renderFpsDisplay(&App, framerate);
 			lastFramerate = framerate;
 		}
