d207920158f1ef7c6628b27a1bf761675a80e04a
ridicul0u5 <andreas.berst@uni-weimar.de>
2015-05-11 09:49:01 +0200

    interval

--- frontend/exp_multitouch/js/setup.js
+++ frontend/exp_multitouch/js/setup.js
@@ -13,6 +13,24 @@ function playerSetup() {
 
 }
 
+
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight)
+      {
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
+ 
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+ 
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
+
+
 //check for input and generate dumb names if no name is set
 function checkName(input) {1429792994379
 
