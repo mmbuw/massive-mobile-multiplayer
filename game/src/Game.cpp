1bee4c66a95c35e2edd0c50e7ba9fb21483e67f9
Tim Weißker <tim.weissker@uni-weimar.de>
2015-05-18 23:14:49 +0200

    Arrange spawn points of new players.

--- game/src/Game.cpp
+++ game/src/Game.cpp
@@ -51,16 +51,14 @@ Player* Game::addNewPlayer(std::string const& name, int number)
 
 	if (teamColor == sf::Color(0, 0, 255))
 	{
-		spawnPosition.x = 480;
-		spawnPosition.y = 600;
+		spawnPosition.x = 0.06 * screenWidth_;
+		spawnPosition.y = 100 + (rand() % (screenHeight_-200));
 		++numPlayersBlue_;
 	}
 	else if (teamColor == sf::Color(255, 0, 0))
 	{
-
-		spawnPosition.x = 480;
-		spawnPosition.y = 300;
-
+		spawnPosition.x = 0.94 * screenWidth_;
+		spawnPosition.y = 100 + (rand() % (screenHeight_-200));
 		++numPlayersRed_;
 	}
 
