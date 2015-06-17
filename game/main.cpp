18c88004f7d3cba01ff38c2607b74435c33506bc
Tim Weißker <tim.weissker@uni-weimar.de>
2015-06-17 17:51:51 +0200

    Add tiny EventDictionary in order to boost performance for summaery.

--- game/main.cpp
+++ game/main.cpp
@@ -13,8 +13,8 @@ int main()
 	srand (time(NULL));
 	sf::Clock Clock;
 
-	sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().width,20), "MMMBall", sf::Style::Fullscreen);
-	//sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().width,20), "MMMBall");
+	//sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().width,20), "MMMBall", sf::Style::Fullscreen);
+	sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().width,20), "MMMBall");
 	window.setVerticalSyncEnabled(true);
 	
 	Game* game = new Game(sf::VideoMode::getDesktopMode().width,
