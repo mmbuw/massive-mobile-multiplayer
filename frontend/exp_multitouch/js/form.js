6a97c547858ae33e16128db3e588c5d523857991
ridicul0u5 <andreas.berst@uni-weimar.de>
2015-07-03 10:41:27 +0200

    cc cleanup

--- frontend/exp_multitouch/js/form.js
+++ frontend/exp_multitouch/js/form.js
@@ -2,25 +2,31 @@ var onLine = false;
 
 window.addEventListener('load', function(){
     document.getElementById('q8').value = navigator.userAgent;
-	document.getElementById("ctimg").src="http://www.studycheck.de/images/institute/normal/uni-weimar.jpg";
 	concheck();
-	
-	setInterval(function() {
-		  concheck();
-	}, 500);
-
 });
+
+window.addEventListener('click', function(){
+	concheck();
+});
+
 
 /*
 *  Reload image for connection dependent forwarding
 */
 function concheck() {
-	document.getElementById("ctimg").src="http://www.studycheck.de/images/institute/normal/uni-weimar.jpg?rand=" + Math.random();
-	if (onLine) {
-		document.getElementById('form').action = "http://www.basicbits.de/area51/mmm/form.php";
-	} else {
-		document.getElementById('form').action = "form.php";
-	}
+	var img = new Image();
+	img.src="http://www.agenda4-online.de/uploads/pics/logo_uni_weimar.jpg?d="+ escape(Date());
+
+	img.onload = doConnectFunction;
+	img.onerror = doNotConnectFunction;
 }
 
 
+function doConnectFunction() {
+		document.getElementById('form').action = "http://www.basicbits.de/area51/mmm/form.php";	
+		console.log("internetz");
+}
+function doNotConnectFunction() {
+		document.getElementById('form').action = "form.php";
+		console.log("no internetz");
+}
