d4b5cca3cc25ccd97cd3bcffe6f861e45b4ecf78
Tim Weißker <me@tim-weissker.de>
2023-03-21 19:07:24 +0100

    Reset configuration.

--- frontend/configuration/configuration.js
+++ frontend/configuration/configuration.js
@@ -1,5 +1,5 @@
 var configuration = {
-  server_ip: '29.4.93.1',  // the IP adress to open the WebSocket connection to
+  server_ip: '29.4.29.1',  // the IP adress to open the WebSocket connection to
   server_port: '53000'	   // the port to use for the WebSocket connection
                            // if you didn't change anything in backend/, the default is 53000
 }
